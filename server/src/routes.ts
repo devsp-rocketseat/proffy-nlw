a8a690155fd57d6ad42fad32fd370dcfc59f9eba
DevSp <contatodevsp@gmail.com>
2020-08-11 11:18:46 -0300

    Separando os controllers das rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,56 +1,10 @@
 import express, { response } from 'express'
-import db from './database/connection'
-import convertHourToMinutos from './utils/convertHourToMinutes'
+import ClassesController from './controllers/ClassesController'
 
 const routes = express.Router()
 
-interface ScheduleItem {
-    week_day: number
-    from: string
-    to: string
-}
+const classesController = new ClassesController()
 
-routes.post('/classes', async (request, response) => {
-
-    const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body
-
-    const trx = await db.transaction()
-
-    try {
-
-        // ---------- users ----------
-        const insertedUsersIds = await trx('users').insert({ name, avatar, whatsapp, bio })
-
-        const user_id = insertedUsersIds[0]
-
-        // ---------- classes ----------
-        const insertedClassesIds = await trx('classes').insert({ subject, cost, user_id })
-
-        const class_id = insertedClassesIds[0]
-
-        // ---------- classes ----------
-        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => ({
-            class_id,
-            week_day: scheduleItem.week_day,
-            from: convertHourToMinutos(scheduleItem.from),
-            to: convertHourToMinutos(scheduleItem.to)
-        }))
-
-        await trx('class_schedule').insert(classSchedule)
-
-        await trx.commit()
-
-        return response.status(201).send()
-
-    } catch (error) {
-
-        await trx.rollback()
-
-        return response.status(400).json({
-            error: 'Erro inesperado ao criar a classe.II'
-        })
-    }
-})
-
+routes.post('/classes', classesController.create)
 
 export default routes
